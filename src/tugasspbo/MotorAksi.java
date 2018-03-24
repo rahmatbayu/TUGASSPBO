49a1720ea0367665e9fb78331e5eeff136a296c4
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-03-24 16:46:01 -0700

    mengisi nilai atribut

--- src/tugasspbo/MotorAksi.java
+++ src/tugasspbo/MotorAksi.java
@@ -15,6 +15,26 @@ public class MotorAksi {
         Motor Honda = new Motor();
         Motor Kawasaki = new Motor();
         
+        
+        yamaha.jenis="Yamaha";
+        yamaha.nama="R6";
+        yamaha.isitorsi="650cc";
+        yamaha.warna="Hitam";
+        yamaha.bahanbakar="Pertamax Turbo";
+        
+        Honda.jenis="Honda";
+        Honda.nama="CBR1000rr";
+        Honda.isitorsi="1000cc";
+        Honda.warna="Putih Merah";
+        Honda.bahanbakar="Pertamax Turbo";
+        
+        Kawasaki.jenis="Kawasaki";
+        Kawasaki.nama="H2R";
+        Kawasaki.isitorsi="1100cc";
+        Kawasaki.warna="Hitam Doff";
+        Kawasaki.bahanbakar="Pertamax Turbo";
+        
+        
     }
     
 }
